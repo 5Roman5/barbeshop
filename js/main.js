823a45b8a117391dd1fb1103f5f1dd645095d217
5Roman5 <roman13101977@gmail.com>
2020-11-22 15:09:35 +0200

    ferstCommitBarbeshop

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#button");
+const content = document.querySelector("#content");
+
+btn.addEventListener("click",btnClick);
+
+function btnClick (){
+    console.log(content.classList);
+    content.classList.remove("content-hidden");
+}
